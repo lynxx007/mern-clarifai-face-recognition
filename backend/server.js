c80bb9c27636273e1e89af9c83cff6be9582fcc1
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-18 20:03:53 +0700

    fixed typo

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,13 @@ import 'dotenv/config'
 import mongoSanitize from 'express-mongo-sanitize'
 import authRoutes from './routes/authRoutes.js'
 import { fileURLToPath } from 'url';
-import path from 'path';
+import path, { dirname } from 'path';
 import { errorHandler, notFound } from './middlewares/errorMiddleware.js'
 import predictRoutes from './routes/imageRoutes.js'
 import cookieParser from 'cookie-parser'
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 await connectDb()
 
@@ -21,7 +20,7 @@ const app = express()
 
 app.use(cookieParser())
 
-app.use("/", express.static(path.join(__dirname, '..', 'client')));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 if (process.env.NODE_ENV === 'development') {
     app.use(cors({
@@ -46,8 +45,8 @@ app.use('/api/v1/image', predictRoutes)
 
 
 // Catch-all route to serve index.html for any other requests
-app.use('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
 })
 
 
