9f2c06a9a8c4fd8b9758ba611fa00e8f79fdfcb1
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-25 11:37:06 +0700

    created clarifai for base64

---
+++ backend/controllers/clarifaiRequests/predictViaBytes.js
@@ -0,0 +1,36 @@
+import { grpc, ClarifaiStub } from "clarifai-nodejs-grpc";
+import 'dotenv/config'
+
+export const clarifaiApiCallBytes = async (base64Image) => {
+    const stub = ClarifaiStub.grpc()
+
+    const metadata = new grpc.Metadata()
+    metadata.set("authorization", `Key ${process.env.PAT}`)
+
+    return new Promise((resolve, reject) => {
+        stub.PostModelOutputs(
+            {
+                user_app_id: {
+                    user_id: process.env.USER_ID,
+                    app_id: process.env.APP_ID
+                },
+                model_id: process.env.MODEL_ID,
+                version_id: process.env.MODEL_VERSION_ID,
+                inputs: [
+                    { data: { image: { base64: base64Image } } }
+                ]
+            },
+            metadata,
+            (err, response) => {
+                if (err) {
+                    reject(err)
+                }
+
+                if (response.status.code !== 10000) {
+                    reject(new Error("Post model outputs failed, status: " + response.status.description));
+                }
+                resolve(response)
+            }
+        )
+    })
+}
