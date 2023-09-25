3bf5cb3b8137d53c0a3bff25432de41e69c8ae1a
Luthfi Rizky <luthfirizky7@gmail.com>
2023-09-25 11:37:46 +0700

    fixed faceDetect controller

--- backend/controllers/auth/faceDetectRegister.js
+++ backend/controllers/auth/faceDetectRegister.js
@@ -1,18 +1,18 @@
+import { clarifaiApiCallBytes } from "../clarifaiRequests/predictViaBytes.js";
 import { clarifaiApiCallUrl } from "../clarifaiRequests/predictViaUrl.js";
 
 
 export const faceDetect = async (req, res) => {
     try {
-        const imageUrl = req.query.imageUrl;
+
+        const imageUrl = req.query.base64Image
+
         if (!imageUrl) {
             return res.status(400).json({ error: 'Missing imageUrl parameter' });
         }
-        const predictedConcepts = await clarifaiApiCallUrl(imageUrl);
+        const predictedConcepts = await clarifaiApiCallBytes(imageUrl)
 
-        res.json({
-            isHuman: true,
-            predictedConcepts
-        })
+        res.json({ isHuman: true, predictedConcepts })
 
     } catch (error) {
         console.error('Error predicting concepts:', error);
