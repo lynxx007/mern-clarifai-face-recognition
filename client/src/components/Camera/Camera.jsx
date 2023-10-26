b3d5f4f00a8e711c4bf07f4161eb49f7e91d91bb
Luthfi Rizky <luthfirizky7@gmail.com>
2023-10-26 08:56:18 +0700

    modified captureImage func

--- client/src/components/Camera/Camera.jsx
+++ client/src/components/Camera/Camera.jsx
@@ -1,12 +1,13 @@
+import { AppContext } from '@/context/AppContext';
 import './Camera-style.css'
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useContext } from 'react';
 
 export const Camera = () => {
     const [webcamStream, setWebcamStream] = useState(null);
     const [capturedImageData, setCapturedImageData] = useState(null);
+    const { submitImgRegister, box } = useContext(AppContext)
     const videoRef = useRef(null);
 
-    console.log(capturedImageData);
 
     // Function to start the webcam
     const startWebcam = async () => {
@@ -28,20 +29,35 @@ export const Camera = () => {
         }
     };
 
+
     // Function to capture the image from the webcam
     const captureImage = () => {
         if (webcamStream) {
             const videoElement = videoRef.current;
             const canvas = document.createElement('canvas');
-            canvas.width = videoElement.videoWidth;
-            canvas.height = videoElement.videoHeight;
+            canvas.width = 800;
+            canvas.height = 600;
             canvas.getContext('2d').drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-            const capturedImageData = canvas.toDataURL('image/jpeg');
+            canvas.toBlob(blob => {
+                if (!blob) return null;
 
-            setCapturedImageData(capturedImageData);
+                if (capturedImageData) URL.revokeObjectURL(capturedImageData);
+
+                // Generate a dynamic filename based on the current date and time
+                const timestamp = Date.now();
+                const fileName = `image-${timestamp}.jpeg`
+
+                // Create a File object from the Blob
+                const file = new File([blob], fileName, { type: 'image/jpeg' });
+                setCapturedImageData(file);
+
+                submitImgRegister(file);
+            });
+
             stopWebcam();
         }
     };
+
 
     useEffect(() => {
         // Clean up the webcam stream when the component unmounts
@@ -70,7 +86,23 @@ export const Camera = () => {
             {capturedImageData && (
                 <div>
                     <h2>Captured Image:</h2>
-                    <img src={capturedImageData} alt="Captured" />
+                    <img id='inputimage' src={URL.createObjectURL(capturedImageData)} alt="Captured" />
+                    {box ? (
+                        box.map((item) => (
+                            <div
+                                key={item.bottomRow}
+                                className='bounding-box'
+                                style={{
+                                    top: item.topRow,
+                                    right: item.rightCol,
+                                    bottom: item.bottomRow,
+                                    left: item.leftCol,
+                                }}
+                            ></div>
+                        ))
+                    ) : (
+                        <div></div>
+                    )}
                 </div>
             )}
         </div>
